2580bf0b49323cbd9f1cbd996c8900ed89e82b37
john <blackcar@dcc.com>
2022-06-23 21:19:43 +0600

    somechanges

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $(".burger").click(function(e) {
+        $(".burger-menu").toggleClass("active");
+        $(".right-tr").toggleClass("active-right-tr");
+        $(".no-tr").toggleClass("active-no-tr");
+        $(".left-tr").toggleClass("active-left-tr");
+    });
+    document.addEventListener("click", function(e) {
+        const burg = document.querySelector(".burger");
+        const click = e.composedPath().includes(burg);
+        const supp = document.querySelector(".fourth-eff");
+        const click2 = e.composedPath().includes(supp);
+        if (!click && !click2) {    
+            $(".burger-menu").removeClass("active");
+            $(".right-tr").removeClass("active-right-tr");
+            $(".no-tr").removeClass("active-no-tr");
+            $(".left-tr").removeClass("active-left-tr");
+        }
+    });
+    $(".fourth-eff").click(function(e) {
+        $(".support-hiden").toggleClass("active-support");
+        $(".counter").toggleClass("active-counter");
+        $(".burger-menu").toggleClass("active-change-size");
+        $(".fourth-eff").toggleClass("fourth-eff-change-color");
+    });
+});
