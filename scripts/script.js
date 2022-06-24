7365955e337c96d359f61ac1a0708f9f9557218e
john <blackcar@dcc.com>
2022-06-24 14:47:53 +0600

    somechanges4

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
-    $(".burger").click(function(e) {
+    $(".burger").click(function() {
         $(".burger-menu").toggleClass("active");
         $(".right-tr").toggleClass("active-right-tr");
         $(".no-tr").toggleClass("active-no-tr");
         $(".left-tr").toggleClass("active-left-tr");
+        $(".burger").click(function() {
+            $(".support-hiden").removeClass("active-support");
+            $(".burger-menu").removeClass("active-change-size");
+            $(".fourth-eff").removeClass("fourth-eff-change-color");
+            $(".counter").removeClass("active-counter");
+        });
     });
     document.addEventListener("click", function(e) {
         const burg = document.querySelector(".burger");
         const click = e.composedPath().includes(burg);
         const supp = document.querySelector(".fourth-eff");
         const click2 = e.composedPath().includes(supp);
-        if (!click && !click2) {    
+        if (!click && !click2) {
             $(".burger-menu").removeClass("active");
             $(".right-tr").removeClass("active-right-tr");
             $(".no-tr").removeClass("active-no-tr");
@@ -18,9 +24,10 @@ $(document).ready(function() {
             $(".support-hiden").removeClass("active-support");
             $(".burger-menu").removeClass("active-change-size");
             $(".fourth-eff").removeClass("fourth-eff-change-color");
+            $(".counter").removeClass("active-counter");
         }
     });
-    $(".fourth-eff").click(function(e) {
+    $(".fourth-eff").click(function() {
         $(".support-hiden").toggleClass("active-support");
         $(".counter").toggleClass("active-counter");
         $(".burger-menu").toggleClass("active-change-size");
